919edaf6270da1a351b883d29458f4d6797566ea
LucasOF23 <lucas.ferreira2306@gmail.com>
2024-05-01 17:11:59 -0300

    Component de Login/Cadastro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
-export default function Root() {
-  return <div>Root</div>;
+"use client";
+
+import React, { useState } from "react"
+import Login from "@/components/Login";
+
+export default function (props) {
+
+  return (
+    <>
+      <button className="btn" onClick={()=>document.getElementById('my_modal').showModal()}>Login</button>
+      <dialog id="my_modal" className="modal">
+        <div className="modal-box p-0 max-w-xs">
+          <Login/>
+        </div>
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
+    </>
+  )
+
 }
