0af8f2e90fb30ff2872049e4549f6872872e118a
Bruno Ricardo <brunomatheus2607@gmail.com>
2024-06-10 01:24:00 -0300

    permite email nulo em informações de contato

--- backend/models/contactinfo.model.js
+++ backend/models/contactinfo.model.js
@@ -6,7 +6,7 @@ class ContactInfo extends Model {};
 
 ContactInfo.init({
 	id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-	email: { type: DataTypes.STRING, allowNull: false },
+	email: { type: DataTypes.STRING, allowNull: true },
 	instagramProfile: { type: DataTypes.STRING, allowNull: true },
 	facebookProfile: { type: DataTypes.STRING, allowNull: true },
 	telephoneNumber: { type: DataTypes.STRING(15), allowNull: true },
