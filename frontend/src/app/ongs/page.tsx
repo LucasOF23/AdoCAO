fe07a595d9b72911be687e16cf92e26297f55e48
LucasOF23 <lucas.ferreira2306@gmail.com>
2024-06-26 14:18:29 -0300

    pagina de ONG

--- frontend/src/app/ongs/page.tsx
+++ frontend/src/app/ongs/page.tsx
@@ -2,9 +2,17 @@
 
 import OngCard from "@/components/OngCard";
 
+import React, { useState } from "react";
+import { createPortal } from "react-dom";
+
 import Navbar2 from "@/components/Navbar2";
 import Lowerbar from "@/components/Lowerbar";
+import Forms from "@/components/Forms_Ong";
+import Sidebar from "@/components/Sidebar2";
 import { ProfileInfo } from "@/types/profile";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 export default function AdoptionPosts() {
     const profileInfos: [ProfileInfo] = [
@@ -40,16 +48,47 @@ export default function AdoptionPosts() {
       },
     ];
 
+    const [isUserAdmin, setButtonVisible] = useState(false);
+
+    const [isAuthVisible, setAuthVisible] = useState(false);
+
+    const closeModal = () => setAuthVisible(false);
+    const openModal = () => setAuthVisible(true);
+  
+    const hiddenClass = isAuthVisible ? "hidden" : "";
+  
+    const [isAuthVisible2, setAuthVisible2] = useState(false);
+  
+    const closeModal2 = () => setAuthVisible2(false);
+    const openModal2 = () => setAuthVisible2(true);
+  
+    const hiddenClass2 = isAuthVisible2 ? "hidden" : "";
+
   return (
     <>
       <Navbar2 />
-      <div className="text-center text-5xl font-bold m-10">Conheça nossas ONG's parceiras!</div>
+        <div className="text-center text-5xl font-bold m-10 mt-14">Conheça nossas ONG's parceiras!</div>
+      
+      
       <div className="p-4 mx-auto screen-max-width grid justify-items-center grid-autofit gap-4">
         {profileInfos.map((info) => (
           info.user_type === "ONG" 
           ? (<OngCard key={info.id} info={info} />)
           : null
         ))}
+        <button onClick={openModal}
+              className={isUserAdmin ? "btn text-9xl text-center content-center border rounded-2xl overflow-hidden w-full max-w-96 hover:shadow-md hover:scale-[101%] transition delay-50" : "hidden"}>
+              +
+            </button>
+            {isAuthVisible &&
+              createPortal(
+                <div className="fixed top-0 left-0 w-full h-full bg-black/40 flex overflow-y-scroll z-20">
+                  <div className="mx-auto my-auto p-4 w-full content-center flex justify-center">
+                    <Forms onClose={closeModal} />
+                  </div>
+                </div>,
+                document.body
+            )}
       </div>
       <Lowerbar />
     </>
