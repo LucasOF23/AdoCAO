772289ef14c1cab0c68e7c21eeb41f9b2ce21011
Bruno Ricardo <brunomatheus2607@gmail.com>
2024-06-27 00:38:43 -0300

    arrumando bug da integração

--- frontend/src/components/OngCard.tsx
+++ frontend/src/components/OngCard.tsx
@@ -32,8 +32,7 @@ export default function OngCard({ info } : OngCardProps) {
 
   return (
     <>
-      <button onClick={ (isSuperAdmin || info.isManager !== undefined ) ? openModal : null}
-        className="border rounded-t-2xl overflow-hidden w-full max-w-96 bg-white hover:shadow-md hover:scale-[101%] transition delay-50">
+      <button onClick={ (isSuperAdmin || info.isManager !== undefined ) ? openModal : null} className="border rounded-t-2xl overflow-hidden w-full max-w-96 bg-white hover:shadow-md hover:scale-[101%] transition delay-50">
         <div className="border rounded-t-2xl overflow-hidden w-full max-w-lg hover:shadow-md hover:scale-[101%] transition delay-50 flex">
           <FontAwesomeIcon
             className="mt-[0.1rem] h-32 text-gray-400"
@@ -49,16 +48,18 @@ export default function OngCard({ info } : OngCardProps) {
             </p>
             <p>Contato: {info.contato.telefone}</p>
           </div>
-        </button>
-        {isModalVisible &&
-          createPortal(
-            <div className="fixed top-0 left-0 w-full h-full bg-black/40 flex overflow-y-scroll z-20">
-              <div className="mx-auto my-auto p-4">
-                <EditONG info={info} onClose={closeModal} />
-              </div>
-            </div>,
-            document.body
-          )}
-      </>
+        </div>
+      </button>
+      {isModalVisible &&
+        createPortal(
+          <div className="fixed top-0 left-0 w-full h-full bg-black/40 flex overflow-y-scroll z-20">
+            <div className="mx-auto my-auto p-4">
+              <EditONG info={info} onClose={closeModal} />
+            </div>
+          </div>,
+          document.body
+        )
+      }
+    </>
   );
 }
