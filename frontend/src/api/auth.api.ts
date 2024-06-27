b15f986e2f60dd9094dfb0e85862bed6fad099f6
Allan Garcia <allangarcia@usp.br>
2024-06-27 15:12:43 -0300

    integração login e signup

--- frontend/src/api/auth.api.ts
+++ frontend/src/api/auth.api.ts
@@ -3,19 +3,25 @@ import { postAnom, post, storeToken } from './general.api';
 async function login(email: string, password: string) {
   const res = await postAnom('signin', { email, password });
 
-  if(res.status === 200)
+  if (res.status === 200)
     storeToken(res.data.token);
-  
+
   return res;
 }
+
+
 
 async function register(name: string, email: string, password: string) {
   const res = await postAnom('signup', { name, email, password });
 
-  if(res.status === 200)
+  if (res.status === 201)
     storeToken(res.data.token);
-  
+
   return res;
+}
+
+function logout() {
+  storeToken(null);
 }
 
 async function changePassword(oldPassword: string, newPassword: string) {
@@ -23,6 +29,6 @@ async function changePassword(oldPassword: string, newPassword: string) {
 }
 
 export default {
-  login, register, changePassword
+  login, register, changePassword, logout
 }
 
